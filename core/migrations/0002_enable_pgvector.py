89efc99f258f5cfabacf34dc8fee6188109faaf1
iimadali610-prog <iimadali610@gmail.com>
2026-07-26 13:29:54 +0300

    Update 0002_enable_pgvector.py

--- core/migrations/0002_enable_pgvector.py
+++ core/migrations/0002_enable_pgvector.py
@@ -1,10 +1,11 @@
 from django.db import migrations
 from pgvector.django import VectorExtension
 
-class Migration(Migration):
+
+class Migration(migrations.Migration):  # <--- 'migrations.Migration' olarak düzeltildi
 
     dependencies = [
-        ('core', '0001_initial'), # Eğer sizde ilk migration adı farklıysa onu yazın
+        ('core', '0001_initial'),  # İlk migration adın neyse o kalmalı
     ]
 
     operations = [
